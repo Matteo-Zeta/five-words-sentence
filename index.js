f593d8ef5d0f7a9a75b5aa926ad88e4ce82fc2ab
Matteo Zappalà <matteozappala@hotmail.it>
2021-02-22 20:58:57 +0100

    Ho scritto una funzione che conta il numero delle parole

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
+function wordCounter () {
+
+    rl.question('Inserisci una frase di almeno 5 parole: ', (answer) => {
+        let wordCount = answer.match(/(\w+)/g).length;
+
+        if (wordCount < 5) {
+            consola.error(`La tua frase è troppo corta, riprova: `);
+            wordCounter();
+        } else {
+            consola.success('Grazie!')
+            rl.close();
+        }      
+      });
+}
+wordCounter();
